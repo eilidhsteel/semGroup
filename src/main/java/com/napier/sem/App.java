c35c7037aef349ab8d73da63972bae456890fbf2
Eilidh Steel <40509130@live.napier.ac.uk>
2022-02-12 13:03:29 +0000

    updated app

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,17 +7,10 @@ import org.bson.Document;
 
 public class App
 {
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args)
     {
-        // Connect to MongoDB
-        MongoClient mongoClient = new MongoClient("localhost",27000
-        );
-
-
+        // Connect to MongoDB on local system - we're using port 27000
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
@@ -35,3 +28,4 @@ public class App
         System.out.println(myDoc.toJson());
     }
 }
+
