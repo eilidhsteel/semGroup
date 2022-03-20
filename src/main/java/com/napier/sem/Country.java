3c9dd0599fb19ec771d35113052467d9517b4f12
AnnieNight <40546104@live.napier.ac.uk>
2022-03-20 09:39:48 +0000

    Created Country Class

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,82 @@
+package com.napier.sem;
+
+/**
+ * Represents a country
+ */
+public class Country {
+    /**
+     * Country's code 3 char long
+     * primark key of the country table
+     */
+    public String country_Code;
+
+    /**
+     * Country's Name
+     */
+    public String country_Name;
+
+    /**
+     * Continent which the country is within
+     */
+    public String continent;
+
+    /**
+     * Region which the country is within
+     */
+    public String region;
+
+    /**
+     * Surface area of the country
+     */
+    public float surface_area;
+
+    /**
+     * Independence year of the country
+     */
+    public int indep_Year;
+
+    /**
+     * Population of the country
+     */
+    public int population;
+
+    /**
+     * Average life expectancy of the country
+     */
+    public float life_Expectancy;
+
+    /**
+     * Gross national product of the country
+     */
+    public float GNP;
+
+    /**
+     * Old gross national product of the country
+     */
+    public float GNP_old;
+
+    /**
+     * Independence year of the country
+     */
+    public String local_Name;
+
+    /**
+     * Government form of the country
+     */
+    public String government_form;
+
+    /**
+     * Head of state for that country
+     */
+    public String head_of_state;
+
+    /**
+     * Capital of the country
+     */
+    public String capital;
+
+    /**
+     * Independence year of the country
+     */
+    public String country_code2;
+}
