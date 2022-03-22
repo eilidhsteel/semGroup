6812c4285ffbe862c50b32e925862b5a8767c95e
Eilidh Steel <40509130@live.napier.ac.uk>
2022-03-22 16:28:09 +0000

    Added counrty unit tests

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -40,7 +40,7 @@ public class AppTest
     }
 
     @Test
-    void printSalaries()
+    void printPopulation()
     {
         ArrayList<City> cities = new ArrayList<City>();
         City city = new City();
@@ -50,5 +50,50 @@ public class AppTest
         city.population = 10;
         cities.add(city);
         app.printPopulation(cities);
+    }
+
+    @Test
+    void printPopulationCounrtyTestNull()
+    {
+        app.printPopulationCountry(null);
+    }
+
+    @Test
+    void printPopulationCountryTestEmpty()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        app.printPopulationCountry(countries);
+    }
+
+    @Test
+    void printPopulationCountryTestContainsNull()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        countries.add(null);
+        app.printPopulationCountry(countries);
+    }
+
+    @Test
+    void printPopulationCountry()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country country = new Country();
+        country.country_name = "UK";
+        country.country_Code = "UKA";
+        country.continent = "Europe";
+        country.region = "Europe";
+        country.surface_area = 1234;
+        country.indep_Year = 1234;
+        country.population = 1234;
+        country.life_Expectancy= 12;
+        country.GNP = 1234;
+        country.GNP_old = 1234;
+        country.local_Name = "UK";
+        country.government_form = "abc";
+        country.head_of_state = "John Smith";
+        //country.capital = NULL;
+        country.country_code2 = "ABC";
+        countries.add(country);
+        app.printPopulationCountry(countries);
     }
 }
