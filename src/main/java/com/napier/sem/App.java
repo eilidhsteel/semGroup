5134e464356e5aeff17ec514015e4c93af18acbe
Eilidh Steel <40509130@live.napier.ac.uk>
2022-04-03 17:35:01 +0100

    Added city report 2 SQL and tested in app

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,7 +16,7 @@ public class App {
             a.connect(args[0], Integer.parseInt(args[1]));
         }
 
-        ArrayList<City> test_cities = a.getAllPopulationsCity(sql.city1);
+        ArrayList<City> test_cities = a.getAllPopulationsCity(sql.city2("Europe"));
         a.printPopulation(test_cities);
 
         //ArrayList<Country> test_countries = a.getAllPopulationsCountry();
--- src/main/java/com/napier/sem/ReportSQL.java
+++ src/main/java/com/napier/sem/ReportSQL.java
@@ -12,4 +12,14 @@ public class ReportSQL {
                                         + "LEFT JOIN country AS ct ON c.CountryCode = ct.Code "
                                         + "ORDER BY c.Population DESC";
 
+    /**
+     * City 2: All the cities in a continent organised by largest population to smallest.
+     */
+    public String city2(String continent){
+        return "SELECT c.Name, c.District, c.Population, country.Name AS country "
+                + "FROM city AS c "
+                + "LEFT JOIN country ON c.CountryCode = country.Code "
+                + "WHERE country.Continent = '" + continent + "' "
+                + "ORDER BY c.Population DESC";
+    }
 }
